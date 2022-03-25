ff1abdc074117b963332af1abed8683a9e10df09
Fantastix80 <jean.vanwittenberge@gmail.com>
2022-03-25 16:55:10 +0100

    Rectification du mdp du compte admin

--- db.sql
+++ db.sql
@@ -62,4 +62,4 @@ CREATE TABLE TRAVEL_PIVO_TAGS
             ON UPDATE RESTRICT
 );
 
-INSERT INTO user(name,email,password) VALUES('admin','admin@admin.com','admin');
+INSERT INTO user(name,email,password) VALUES('admin','admin@admin.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
