1a115d6b612f8324301cf9ca7a87040e0e80e3e6
Fantastix80 <jean.vanwittenberge@gmail.com>
2022-04-04 14:59:36 +0200

    Style de la page login

--- layouts/admin/header.php
+++ layouts/admin/header.php
@@ -7,7 +7,6 @@
     <title>JetLag</title>
 
     <!-- font awesome cdn link  -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
